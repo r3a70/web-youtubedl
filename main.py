be300edcc5fe377bde722f0240d7c0608bcdab7e
r3a96 <r3a96.info@gmail.com>
2022-04-12 11:25:00 +0430

    edit main.py file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from backend.routers import youtube
 
 app = FastAPI()
@@ -8,6 +9,7 @@ app = FastAPI()
 app.include_router(youtube.router)
 
 templates = Jinja2Templates(directory="frontend")
+app.mount("/frontend/style/", StaticFiles(directory="frontend/style"), name="frontend")
 
 
 @app.get("/", response_class=HTMLResponse)
