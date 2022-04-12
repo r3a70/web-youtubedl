396e08942454d0d3025ccef5d1adb268f51298dc
r3a96 <r3a96.info@gmail.com>
2022-04-12 16:49:39 +0430

    fix some bug

--- backend/routers/downloader.py
+++ backend/routers/downloader.py
@@ -16,15 +16,20 @@ templates = Jinja2Templates(directory="frontend")
 @router.get("/", status_code=status.HTTP_200_OK)
 async def download_from_youtube(url: str, request: Request):
 
-    video_type = await find_url_destination.find(url)
-    if video_type is None:
+    try:
+        video_type = await find_url_destination.find(url)
+        if video_type is None:
+            return RedirectResponse("/")
+        elif video_type == "youtube":
+            video_info = await youtube_extractor.yt_extractor(url)
+        elif video_type == "tiktok":
+            video_info = await tiktok_extractor.tk_extractor(url)
+        elif video_type == "twitter":
+            video_info = await twitter_extractor.tw_extractor(url)
+        else:
+            return RedirectResponse("/")
+    except Exception:
         return RedirectResponse("/")
-    elif video_type == "youtube":
-        video_info = await youtube_extractor.yt_extractor(url)
-    elif video_type == "tiktok":
-        video_info = await tiktok_extractor.tk_extractor(url)
-    elif video_type == "twitter":
-        video_info = await twitter_extractor.tw_extractor(url)
 
     return templates.TemplateResponse("youtube.html", {"request": request, "info": video_info})
 
