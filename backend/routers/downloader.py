3192a90b9176d3af872af3cf780ed58ec917a886
r3a96 <r3a96.info@gmail.com>
2022-04-16 15:27:52 +0430

    add get video info as json in downloader.py file

--- backend/routers/downloader.py
+++ backend/routers/downloader.py
@@ -41,3 +41,27 @@ async def download_from_youtube(url: str, request: Request, db: Session = Depend
     return templates.TemplateResponse("youtube.html", {"request": request, "info": video_info})
 
 
+@router.get("/get", status_code=status.HTTP_200_OK)
+async def download_from_youtube(url: str, request: Request, db: Session = Depends(database.get_db)):
+
+    db_query = db.query(models.Uses).filter(models.Uses.urls == url).first()
+
+    try:
+        video_type = await find_url_destination.find(url)
+        if video_type is None:
+            return RedirectResponse("/")
+        elif video_type == "youtube":
+            video_info = await youtube_extractor.yt_extractor(url)
+        elif video_type == "tiktok":
+            video_info = await tiktok_extractor.tk_extractor(url)
+        elif video_type == "twitter":
+            video_info = await twitter_extractor.tw_extractor(url)
+        else:
+            return RedirectResponse("/")
+    except Exception:
+        return RedirectResponse("/")
+    if not db_query:
+        new_uses = models.Uses(urls=url)
+        db.add(new_uses)
+        db.commit()
+    return {"info": video_info}
